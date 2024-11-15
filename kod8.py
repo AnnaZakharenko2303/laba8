d4ec82f4d68e46c1b2d2a5409e3fc9b7d55c139f
Your Name <you@example.com>
2024-11-15 15:31:52 +0700

    Итерация 1: Объявлены все классы и основные методы с заглушками

---
+++ kod8.py
@@ -0,0 +1,23 @@
+class MatrixProcessor:
+    def __init__(self):
+        self.matrix = []  # Инициализация матрицы
+
+    def input_matrix(self):
+        pass  # Заглушка для ввода матрицы
+
+    def generate_matrix(self):
+        pass  # Заглушка для генерации случайной матрицы
+
+    def process_matrix(self):
+        pass  # Заглушка для обработки матрицы
+
+    def output_matrix(self):
+        pass  # Заглушка для вывода матрицы
+
+
+def main_menu():
+    pass  # Заглушка для главного меню
+
+
+if __name__ == "__main__":
+    main_menu()
